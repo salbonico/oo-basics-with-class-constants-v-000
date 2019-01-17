4b87446413453ef1d64912c041a0a8436d241c01
Steve Albonico <salbonico@gmail.com>
2019-01-17 02:22:19 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,8 @@
 class Book
 GENRE = []
-  
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
 
   def initialize(title)
     @title = title
@@ -11,5 +11,9 @@ GENRE = []
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+
+def genre=
+  @genre=genre
+  GENRE = genre
 
 end
